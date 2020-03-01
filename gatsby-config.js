f25abf54140f83f3bb7565913701453137376a12
Indraneel Purohit <i.m.indraneel@gmail.com>
2020-03-01 10:41:39 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Indraneel Purohit`,
+    description: `Indraneel's site`,
     siteUrl: `http://indraneel.org/`,
     social: {
       twitter: `indraneel24`,
