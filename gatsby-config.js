72761c84ccf10213f7f276163d0f3cc2288bda22
Indraneel Purohit <i.m.indraneel@gmail.com>
2021-03-28 21:25:21 -0400

    remove manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,17 +55,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-      },
-    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
